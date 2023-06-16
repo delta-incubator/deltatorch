9e574e234570761b3e8a824b5af63459f46cffc6
Udhayaraj Sivalingam <udhayaraj12@gmail.com>
2023-06-16 16:49:22 +0200

    support pytorch dataloader kw args

--- deltatorch/pytorch.py
+++ deltatorch/pytorch.py
@@ -1,4 +1,4 @@
-from typing import Optional, Callable, List, Any
+from typing import List
 
 from torch.utils.data import DataLoader
 
@@ -11,14 +11,46 @@ def create_pytorch_dataloader(
     id_field: str,
     fields: List[FieldSpec],
     batch_size: int = 32,
-    collate_fn: Optional[Callable[[List], Any]] = None,
     use_fixed_rank: bool = False,
     rank: int = None,
     num_ranks: int = None,
     num_workers: int = 2,
     shuffle: bool = False,
     drop_last: bool = False,
+    **pytorch_dataloader_kwargs
 ):
+    """Create a PyTorch DataLoader.
+
+    This method will do the following two steps:
+      1) Create an iterable dataset out of delta tables.
+      2) Create a PyTorch DataLoader based on the itearable dataset created in (1)
+
+    :param path: Path to the DeltaLake table
+    :param id_field: Autoincrement ID field.delta table must have an autoincrement ID
+    field.This field is used by deltatorch for sharding and parallel data loading
+    :param fields: List of  `detatorch.FieldSpec` used for training
+    :param batch_size: The number of items to return per batch. Default ``32``.
+    :param rank: Rank of the current process within the current distributed
+            group. This value is set by ``torch.distributed.get_rank()``
+            when ``use_fixed_rank`` is set to `False`.
+    :param num_ranks: number of processes in the current distributed process group.
+           This value is set by ``torch.distributed.get_world_size()``
+           when ``use_fixed_rank`` is set to `False`.
+    :param use_fixed_rank: If this values is set to `True`, `rank` and `num_ranks`
+    should be set explicitly by user
+    :param num_workers: An int for the number of workers to use in dataloader
+    :param shuffle: set to ``True`` to have the data reshuffled
+            inside the record batches (default: ``False``).
+    :param drop_last: set to ``True`` to drop the last incomplete batch,
+            if the dataset size is not divisible by the batch size. If ``False`` and
+            the size of dataset is not divisible by the batch size, then the last batch
+            will be smaller. (default: ``False``)
+    :param pytorch_dataloader_kwargs: arguments for `torch.utils.data.DataLoader`,
+        exclude these arguments: ``batch_size``, ``num_workers``, ``shuffle``,
+        ``drop_last``.
+
+    :return: `torch.utils.data.DataLoader` object.
+    """
     dataset = IDBasedDeltaDataset(
         path,
         id_field,
@@ -37,6 +69,6 @@ def create_pytorch_dataloader(
         batch_size=batch_size,
         shuffle=False,
         num_workers=num_workers,
-        collate_fn=collate_fn,
         drop_last=drop_last,
+        **pytorch_dataloader_kwargs
     )
